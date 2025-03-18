4f6aa03c6bd1daa274645e4e40b5ff9b11495f5e
Martin Bopko <forcemarci@gmail.com>
2025-03-18 13:40:34 +0100

    Ambulance waiting list CRUD operations

--- src/components/cv2-ambulance-wl-list/cv2-ambulance-wl-list.tsx
+++ src/components/cv2-ambulance-wl-list/cv2-ambulance-wl-list.tsx
@@ -45,15 +45,19 @@ export class Cv2AmbulanceWlList {
         ? <div class="error">{this.errorMessage}</div>
         :
           <md-list>
-            {this.waitingPatients.map((patient, index) =>
-              <md-list-item onClick={ () => this.entryClicked.emit(index.toString())}>
-                <div slot="headline">{patient.name}</div>
+            {this.waitingPatients.map((patient) =>
+              <md-list-item onClick={ () => this.entryClicked.emit(patient.id)}>
+                      <div slot="headline">{patient.name}</div>
                 <div slot="supporting-text">{"Predpokladaný vstup: " + patient.estimatedStart?.toLocaleString()}</div>
                   <md-icon slot="start">person</md-icon>
               </md-list-item>
             )}
         </md-list>
         }
+            <md-filled-icon-button class="add-button"
+      onclick={() => this.entryClicked.emit("@new")}>
+      <md-icon>add</md-icon>
+    </md-filled-icon-button>
       </Host>
     );
   }
